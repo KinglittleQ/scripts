453a1f37e9f8052f535e93ad2ddb77b87aa7c47c
Check Deng <checkdeng0903@gmail.com>
2021-01-16 16:57:50 +0800

    Add a script to set git identity

---
+++ set_gitconfig.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# ========================================================
+# File: set_gitconfig.sh
+# -----
+# Author: Chengqi Deng
+# Email: checkdeng0903@gmail.com
+# =======================================================
+
+git config --global user.email "checkdeng0903@gmail.com"
+git config --global user.name "Chengqi Deng"
+
