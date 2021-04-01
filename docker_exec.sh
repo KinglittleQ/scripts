8e705423bb15e81ed42eb0de2e1a03a57f2b7b25
Check Deng <checkdeng0903@gmail.com>
2021-04-02 00:16:50 +0800

    Update

--- docker_exec.sh
+++ docker_exec.sh
@@ -7,11 +7,15 @@
 # Email: checkdeng0903@gmail.com
 # =======================================================
 
+SCRIPTS_DIR=$(cd "$(dirname "$0")" && pwd)
+source $SCRIPTS_DIR/env.sh
 
+TAG="v0.4"
 SHELL="zsh"
 WORK_DIR="$(pwd)"
 BASE_DIR="$(basename $WORK_DIR)"
-DOCKER_NAME="dev_in_$BASE_DIR"
+
+DOCKER_NAME=$(filter "dev_in_$BASE_DIR:$TAG")
 
 function main() {
   docker exec -it \
